f2395b946eb1fe5952b02309762befd514d2d1f2
Adam Coffman <coffman.adam@gmail.com>
2009-02-02 17:14:11 -0600

    initial commit

---
+++ SearchableADT.h
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class SearchableADT{
+
+	public:
+		virtual int loadFromFile(string filename) = 0;
+		virtual void clear() = 0;
+		virtual void insertEntry(T value) = 0;
+		virtual void deleteEntry(T value) = 0;
+		virtual bool isThere(T value) = 0;
+		virtual int numEntries() = 0;
+};
